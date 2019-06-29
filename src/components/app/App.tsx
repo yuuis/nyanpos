2f3c634ec0da7884bb7d58e028b7668f3c60aecb
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-29 20:08:14 +0900

    add Apps

---
+++ src/components/app/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {User} from "../../models/User";
+
+export interface AppProps {
+  users: User[]
+  selected: string
+}
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
