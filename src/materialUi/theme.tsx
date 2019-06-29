3ba8d4df6a1b40ebeb276ae03b1b0b53ead151b6
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-30 03:09:19 +0900

    add theme

---
+++ src/materialUi/theme.tsx
@@ -0,0 +1,18 @@
+import {createMuiTheme} from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#a0ffaa",
+      main: "#6be57a",
+      dark: "#30b24c",
+      contrastText: "#000000"
+    },
+    secondary: {
+      light: "#8e8e8e",
+      main: "#616161",
+      dark: "#373737",
+      contrastText: "#ffffff"
+    },
+  },
+});
