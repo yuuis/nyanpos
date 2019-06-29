10cf82f9c1dbe957dcbd5bec8be9f0960e7cafb0
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-29 20:09:22 +0900

    add reducers

---
+++ src/reducers/common.tsx
@@ -0,0 +1,51 @@
+import uuid from "uuid";
+import {GlobalState} from "../models/GlobalState";
+import {User} from "../models/User";
+import {Post} from "../models/Post";
+import {Crap} from "../models/Crap";
+
+const initialUsers: User[] = [
+  {
+    uuid: uuid.v4.toString(),
+    name: 'クロ',
+    image_url: 'https://4.bp.blogspot.com/-RjoXyQHx4Ak/UnyGN4WF7fI/AAAAAAAAadU/HsABLjETKSM/s400/neko_ushiro.png',
+    points: 400,
+  },
+  {
+    uuid: uuid.v4.toString(),
+    name: 'ぶち',
+    image_url: 'https://2.bp.blogspot.com/-OJI1nrtWGpY/UnyGKzzKl4I/AAAAAAAAacY/9xIDbTNbQ9Y/s400/neko_furikaeri.png',
+    points: 400,
+  },
+  {
+    uuid: uuid.v4.toString(),
+    name: 'トラ',
+    image_url: 'https://4.bp.blogspot.com/-nzGNxI1lJsc/UnyGMotWQxI/AAAAAAAAadA/1NeQYPIZCmk/s400/neko_punch.png',
+    points: 400,
+  },
+  {
+    uuid: uuid.v4.toString(),
+    name: 'ミケ',
+    image_url: 'https://3.bp.blogspot.com/-I5omlyOiw04/W1vg2o3hHgI/AAAAAAABNr4/_PpEM8ZROX42aTaNNZQK2_p2JT7Bfp8zgCLcBGAs/s400/animal_chara_computer_neko.png',
+    points: 400,
+  },
+];
+
+const initialPosts: Post[] = [];
+const initialCraps: Crap[] = [];
+
+export const initialGlobalState: GlobalState = {
+  posts: initialPosts,
+  users: initialUsers,
+  craps: initialCraps,
+  currentUser: initialUsers[0]
+};
+
+export type GlobalState = typeof initialGlobalState
+
+export interface Action {
+  type: string
+  user?: User
+  post?: Post
+  crap?: Crap
+}
---
+++ src/reducers/reducer.tsx
@@ -0,0 +1,54 @@
+import { GlobalState } from '../models/GlobalState'
+import {CHANGE_CURRENT_USER, CREATE_CLAP, CREATE_POST, INIT} from "../constatnts/actionTypes";
+import {Action, initialGlobalState} from "./common";
+
+export const reducer = (state: GlobalState, action: Action) => {
+  switch (action.type) {
+    case INIT:
+      return initialGlobalState;
+    case CHANGE_CURRENT_USER:
+      return action.user
+        ? {
+          ...state,
+          currentUser: action.user
+        }
+        : state;
+    case CREATE_POST:
+      return action.post
+        ? {
+          ...state,
+          posts: [...state.posts, action.post]
+        }
+        : state;
+    case CREATE_CLAP:
+      if (action.crap) { return state; }
+
+      state.users.splice(state.users.indexOf(action.crap.post.send_user), 1);
+      state.users.splice(state.users.indexOf(action.crap.post.receive_user), 1);
+      state.users.splice(state.users.indexOf(action.crap.user), 1);
+
+      return action.crap
+        ? {
+          ...state,
+          craps: [...state.craps, action.crap],
+          users: [
+            ...state.users,
+            {
+              ...action.crap.post.send_user,
+              points: action.crap.post.send_user.points++
+            },
+            {
+              ...action.crap.post.receive_user,
+              points: action.crap.post.receive_user.points++
+            },
+            {
+              ...action.crap.user,
+              points: action.crap.user.points++
+            }
+          ]
+        }
+        : state;
+    default:
+      return state;
+  }
+};
